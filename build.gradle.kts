276387a4a3541683346e2e303d41916aae2aecb8
Miky Delzar <miguel.delzar@gmail.com>
2026-09-15 23:11:30 -0600

    fix: manage service dependencies with Spring Boot BOM

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,12 @@ plugins {
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 
 dependencies {
+    implementation(platform(libs.spring.boot.bom))
     implementation(libs.spring.boot.starter.jdbc)
     implementation(libs.spring.boot.starter.data.redis)
     implementation(libs.spring.boot.starter.web)
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.validation)
     implementation(libs.httpclient5)
     implementation(libs.resilience4j.spring.boot3)
     implementation(libs.caffeine)
